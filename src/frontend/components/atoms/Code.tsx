cad8ef058a3fe94b3b0b49b4f509ce5e6094744e
Stefan Girlich <stefan.girlich@gmail.com>
2023-04-08 19:50:10 +0200

    improve "code"-style paragraph readability on win 10

--- src/frontend/components/atoms/Code.tsx
+++ src/frontend/components/atoms/Code.tsx
@@ -4,7 +4,8 @@ import styled from 'styled-components'
 const Code = styled.pre`
     padding: 8px;
     margin-bottom: 32px;
-    font-size: 0.6rem;
+    font-size: 0.7rem;
+    line-height: 1.3rem;
     letter-spacing: 0.025rem;
     padding: 12px;
     color: ${colors.list.itemText.idle}; // TODO
