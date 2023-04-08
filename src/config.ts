c0a9cd201fbbcf35f9d9174b80628327b1e61268
Stefan Girlich <stefan.girlich@gmail.com>
2023-04-08 18:15:29 +0200

    add absolute Windows path for workspace directory

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // TODO move to @src/common/config/somethingsomething.ts
-export const MODS_DIR = 'userdata/MODS' // TODO make this user-selectable
-export const MOD_RESOURCES_DIR = 'userdata/resources' // TODO make this user-selectable or automatic (e.g. AppData)
+export const ABSOLUTE_BASE_PATH= 'C:\\Users\\Arno\\projects\\ams2-content-manager\\'
+export const MODS_DIR = `${ABSOLUTE_BASE_PATH}userdata\\MODS` /* TODO make this user-selectable*/ 
+export const MOD_RESOURCES_DIR = `${ABSOLUTE_BASE_PATH}userdata\\resources` // TODO make this user-selectable or automatic (e.g. AppData)
 export const SUPPORTED_ARCHIVE_MIME_TYPES = ['application/x-7z-compressed']
 export const WIN_LINEBREAK = '\r\n'
